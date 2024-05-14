316813648136cb0a8f88c60a2fb19b68468aae23
Nahum <toodoo1703@gmail.com>
2024-05-13 21:31:05 -0600

    1.0

--- redirectionsManager.js
+++ redirectionsManager.js
@@ -1,4 +1,28 @@
-function SociedadTecnológica()
+function SociedadTecnologica()
 {
     window.location.href = "Sociedad tecnológica/sociedad.html"
+}
+function SociedadTecnologica2()
+{
+    window.location.href ="sociedad.html"
+}
+function Mundo()
+{
+    window.location.href = "Mundo.html"
+}
+function MainMenu()
+{
+    window.location.href ="../index.html"
+}
+function MainMenu2()
+{
+    window.location.href ="index.html"
+}
+function Defense()
+{
+    window.location.href = "defense.html"
+}
+function Auto()
+{
+    window.location.href = "auto.html"
 }
